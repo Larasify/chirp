f4120cdc88dacac93e0a70f9563d08e33df47707
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-05-29 09:22:28 +0100

    Profile page layout

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, type NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
 import { LoadingPage } from "~/components/loading";
+import Image from "next/image";
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data, isLoading } = api.profile.getUserByUsername.useQuery({
@@ -15,10 +16,21 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   return (
     <>
       <Head>
-        <title>{data.username}'s Profile</title>
+        <title>{data.username}</title>
       </Head>
-      <PageLayout>  
-        <div>{data.username}</div>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s Profile Image`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400"></div>
       </PageLayout>
     </>
   );
