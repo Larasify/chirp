82afb1b45cca7eb4f2910fe1e924ee5c6e310829
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-05-29 09:43:06 +0100

    profile page done

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -4,6 +4,16 @@ import { api } from "~/utils/api";
 import { LoadingPage } from "~/components/loading";
 import Image from "next/image";
 
+const ProfileFeed = (props: {userId:string}) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({userId:props.userId});
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length===0) return <div>User has not posted</div>;
+
+  return <div className="flex flex-col">{data.map((fullPost) => (<PostView {...fullPost} key={fullPost.post.id} />))}</div>
+}
+
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data, isLoading } = api.profile.getUserByUsername.useQuery({
     username: username,
@@ -31,6 +41,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <div className="h-[64px]"></div>
         <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
         <div className="w-full border-b border-slate-400"></div>
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
@@ -41,6 +52,7 @@ import { prisma } from "~/server/db";
 import superjson from "superjson";
 import { createServerSideHelpers } from "@trpc/react-query/server";
 import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createServerSideHelpers({
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -7,11 +7,38 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
-
-
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
+import type { Post } from "@prisma/client";
+
+const addUserDataToPosts = async (posts: Post []) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+  ).map(filterUserForClient);
+
+  return posts.map((post) => {
+    {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author || !author.username) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author not found",
+        });
+      }
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    }
+  });
+}
 
 // Create a new ratelimiter, that allows 3 requests per 60  seconds
 const ratelimit = new Ratelimit({
@@ -21,6 +48,7 @@ const ratelimit = new Ratelimit({
 });
 
 export const postsRouter = createTRPCRouter({
+  
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
@@ -29,35 +57,33 @@ export const postsRouter = createTRPCRouter({
       },
     });
 
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100,
-      })
-    ).map(filterUserForClient);
-
-    return posts.map((post) => {
-      {
-        const author = users.find((user) => user.id === post.authorId);
-        if (!author || !author.username) {
-          throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: "Author not found",
-          });
-        }
-        return {
-          post,
-          author: {
-            ...author,
-            username: author.username,
-          },
-        };
-      }
-    });
+    return await addUserDataToPosts(posts);
+    
   }),
 
+  getPostsByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(({ ctx, input }) =>
+      ctx.prisma.post.findMany({
+        where: {
+          authorId: input.userId,
+        },
+        take: 100,
+        orderBy: {
+          createdAt: "desc",
+        },
+      }).then(addUserDataToPosts)
+    ),
+
   create: privateProcedure
-    .input(z.object({ content: z.string().min(1, "Please put at least 1 character.").max(144, "Please use less than 144 characters.") }))
+    .input(
+      z.object({
+        content: z
+          .string()
+          .min(1, "Please put at least 1 character.")
+          .max(144, "Please use less than 144 characters."),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
 
