5a9e028d87485b30cba473105dfd5f1fbac3bd3c
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-05-30 08:41:59 +0100

    working redirects with next router

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -3,18 +3,23 @@ import Image from "next/image";
 import Link from "next/link";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { useRouter } from "next/router";
+
+
 
 dayjs.extend(relativeTime);
 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 export const PostView = (props: PostWithUser) => {
   const { post, author } = props;
+  const router = useRouter();
+
   const handleCardClick = () => {
-    window.location.href = `/post/${post.id}`;
+    router.push(`/post/${post.id}`);
   };
   const handleProfileClick = (event: React.MouseEvent) => {
     event.stopPropagation();
-    window.location.href = `/@${author.username}`;
+    router.push(`/@${author.username}`);
   };
 
   return (
@@ -23,31 +28,17 @@ export const PostView = (props: PostWithUser) => {
       onClick={handleCardClick}
       className=" flex gap-3 border-b border-slate-400 p-4 hover:cursor-pointer"
     >
-      <Link
-        href={`/@${author.username}`}
-        onClick={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <Image
-          src={author.profileImageUrl}
-          alt={`@${author.username}'s profile image`}
-          className="h-14 w-14 rounded-full"
-          onClick={handleProfileClick}
-          width={56}
-          height={56}
-        />
-      </Link>
+      <Image
+        src={author.profileImageUrl}
+        alt={`@${author.username}'s profile image`}
+        className="h-14 w-14 rounded-full"
+        onClick={handleProfileClick}
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col gap-1">
         <div className="flex gap-1 text-slate-300">
-          <Link
-            href={`/@${author.username}`}
-            onClick={(event) => {
-              event.preventDefault();
-            }}
-          >
-            <span onClick={handleProfileClick}>{`@${author.username}`}</span>
-          </Link>
+          <span onClick={handleProfileClick}>{`@${author.username}`}</span>
           <span className="font-thin">{` · ${dayjs(
             post.createdAt
           ).fromNow()}`}</span>
