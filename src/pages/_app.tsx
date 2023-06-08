13da270e63f428bcc5f715aecef0ea0cc127440c
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-08 11:25:45 +0300

    manifest and branding

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,54 @@ import { Toaster } from "react-hot-toast";
 import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const description = "Chirp away in this Twitter clone!";
+  const title = "Chirp";
+  const imageMetaURL = "https://chirp-larasify.vercel.app/bird.png";
+
+  
   return (
     <ClerkProvider {...pageProps}>
       <Head>
-        <title>Chirp</title>
-        <meta name="description" content="waow based" />
+      <title>title</title>
+        <meta name="description" content={description} />
         <link rel="icon" href="/turtle.svg" />
+
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageMetaURL} />
+        <meta name="twitter:image" content={imageMetaURL} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="theme-color" content="#000000" />
+
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/bird.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/bird.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/bird.png"
+        />
+        <link rel="manifest" href="/site.webmanifest?v=2" />
+        <link rel="shortcut icon" href="/turtle.svg" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Chirp"
+        />
+        <meta
+          name="application-name"
+          content="Chirp"
+        />
       </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
